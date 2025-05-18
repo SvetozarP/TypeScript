692265271976138eda6011d5231dbc34cd16f6dd
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-05-18 15:18:25 +0100

    Task4 complete

---
+++ 02 Introduction Ex/04 convertArrays.ts
@@ -0,0 +1,13 @@
+function convertArrays(words: string[]): [string, number] {
+    
+    const text = words.join('');
+    
+    return [
+        text,
+        text.length
+    ]
+}
+
+console.log(convertArrays(['How', 'are', 'you?']));
+
+console.log(convertArrays(['Today', ' is', ' a ', 'nice', ' ', 'day for ', 'TypeScript']));
