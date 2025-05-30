8bfec9da5c20643aa608235f1c624cc49b762356
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-05-30 20:26:41 +0100

    Task14 complete

---
+++ 06 TypeScript OOP EX/14 OverloadedCalculator.ts
@@ -0,0 +1,60 @@
+class Calculator {
+    
+    public calculate(operation: 'power' | 'log', a: number, b: number): number;
+    public calculate(operation: 'add' | 'subtract' | 'multiply' | 'divide', a: number, b: number, c?: number, d?: number): number;
+
+    public calculate(
+        operation: 'power' | 'log' | 'add' | 'subtract' | 'multiply' | 'divide',
+        a: number, 
+        b: number, 
+        c?: number, 
+        d?: number
+    ): number {
+        
+        let validNums = [a, b, c, d].filter(el => el !== undefined);
+
+        switch(operation) {
+            case 'power': 
+                return a ** b
+            case 'log': 
+
+                if (a <= 0 || b<= 0 ) { 
+                    throw new Error('Invalid a or b') 
+                }
+                
+                return Math.log(a) / Math.log(b)
+            
+            case 'add': 
+                return validNums.reduce((acc, val) => acc + val)
+            case 'subtract': 
+                return validNums.reduce((acc, val) => acc - val)
+            case 'multiply': 
+                return validNums.reduce((acc, val) => acc * val)
+            case 'divide': 
+                validNums = validNums.filter(el => el !== 0)
+                return validNums.reduce((acc, val) => acc / val)
+            
+        } return 0
+    }
+
+}
+
+
+const calc = new Calculator();
+console.log(calc.calculate('power', 2, 3));
+console.log(calc.calculate('power', 4, 1/2));
+console.log(calc.calculate('log', 8, 2));
+console.log(calc.calculate('add', 10, 5));
+console.log(calc.calculate('add', 10, 5, 3));
+console.log(calc.calculate('subtract', 10, 5));
+console.log(calc.calculate('multiply', 2, 3, 4));
+console.log(calc.calculate('divide', 100, 5, 2, 2));
+
+
+
+// Errors - not matching signatures
+// const calc1 = new Calculator();
+// console.log(calc.calculate('power', 2, 3, 2));
+// console.log(calc.calculate('add', 2));
+// console.log(calc.calculate('log', 2, 3, 4, 5)); 
+// console.log(calc.calculate('multiply', 2, 3, 4, 5, 6));
