21a4f749349dc953716e23c07b05b9ab1293f881
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-06-07 19:08:49 +0100

    Task 2 solved

---
+++ 10 TS Decorators Ex/02 validateAge.ts
@@ -0,0 +1,29 @@
+function AgeValidator(target: any, methodName: string, descriptor: PropertyDescriptor) {
+    const origMethod = descriptor.set
+    descriptor.set = function(val: number) {
+        if (val < 1 || val > 200 || !val) {
+            throw new Error('Error: Age must be between 1 and 200')
+        }
+
+        origMethod?.call(this, val)
+    }
+
+    return descriptor
+}
+
+class Age {
+    private _age!: number;
+    constructor(age: number){ 
+        this.age = age;
+     }
+
+    @AgeValidator
+    set age(val: number){ this._age = val; }
+    get age() { return this._age;  }
+}
+
+
+let ageVal = new Age(10);
+ageVal.age = -10;
+console.log(ageVal.age);
+
